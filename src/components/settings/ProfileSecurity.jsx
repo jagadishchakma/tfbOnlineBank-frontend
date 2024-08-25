31e959fe6734b666cad320583a876b44dadf0eaf
jagadishchakma <jagadishchakma401@gmail.com>
2024-08-25 16:59:44 +0600

    latest commit

--- src/components/settings/ProfileSecurity.jsx
+++ src/components/settings/ProfileSecurity.jsx
@@ -6,21 +6,21 @@ const ProfileSecurity = () => {
       <form action="" className="w-50">
         <div className="d-flex align-items-center gap-3">
           <div className="form-group w-100">
-            <label htmlFor="old_pass">Old Password</label> <br/>
+            <label htmlFor="old_pass">Old Password </label> <br/>
             <input type="password" className="w-100" name="old_pass" id="old_pass" placeholder="Old Password"/>
           </div>
         </div>
 
         <div className="d-flex align-items-center gap-3">
           <div className="form-group w-100">
-            <label htmlFor="new_pass">New Password</label> <br/>
+            <label htmlFor="new_pass">New Password </label> <br/>
             <input type="password" className="w-100" name="new_pass" id="new_pass" placeholder="New Password"/>
           </div>
         </div>
 
         <div className="d-flex align-items-center gap-3">
           <div className="form-group w-100">
-            <label htmlFor="new_confirm_pass">New Confirm Password</label> <br/>
+            <label htmlFor="new_confirm_pass"> New Confirm Password </label> <br/>
             <input type="password" className="w-100" name="new_confirm_pass" id="new_confirm_pass" placeholder="New Confirm Password"/>
           </div>
         </div>
